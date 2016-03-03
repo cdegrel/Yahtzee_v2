f69d6b0fcc28e6020117edc7682ebebc0a7824f8
nico <nico@sr388.home>
2016-03-03 05:59:27 +0100

    ajout liste des coup + verif

--- src/Yahtzee/util/IA.java
+++ src/Yahtzee/util/IA.java
@@ -5,6 +5,7 @@ import Yahtzee.model.Model;
 import Yahtzee.view.IATabController;
 import javafx.scene.control.Tab;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class IA extends Joueur {
@@ -12,10 +13,25 @@ public class IA extends Joueur {
 	public De de;
 	public Model model;
 	protected IATabController controller;
+	protected ArrayList<String> listCoup;
 
 	public IA(Tab tab, int numIA) {
 		super(tab, numIA, true);
 		controller = Main.initTabIALayout(tab, numIA);
+		listCoup = new ArrayList<>();
+		listCoup.add("chance");
+		listCoup.add("un");
+		listCoup.add("deux");
+		listCoup.add("trois");
+		listCoup.add("quatre");
+		listCoup.add("cinq");
+		listCoup.add("six");
+		listCoup.add("brelan");listCoup.add("carre");
+		listCoup.add("full");listCoup.add("petiteSuite");
+		listCoup.add("grandeSuite");listCoup.add("yahtzee");listCoup.add("chance");
+
+
+
 	}
 
 	public IATabController getIAController() {
@@ -30,41 +46,49 @@ public class IA extends Joueur {
 		de.jette();
 		Arrays.sort(de.DesGen);
 
-		if (model.yahtzee(de.DesGen) == 50) {
+		if(listCoup.size()!=0) {
 
-			this.setScoreSomme(50);
-			this.setscoreTotal(50);
+			if (listCoup.contains("yahtzee") && model.yahtzee(de.DesGen) == 50) {
 
-		} else if (model.grandeSuite(de.DesGen) == 40) {
+				this.setScoreSomme(50);
+				this.setscoreTotal(50);
+				listCoup.remove("yahtzee");
 
-			this.setScoreSomme(50);
-			this.setscoreTotal(50);
+			} else if (listCoup.contains("grandeSuite") && model.grandeSuite(de.DesGen) == 40) {
 
-		} else if (model.petiteSuite(de.DesGen) == 30) {
+				this.setScoreSomme(50);
+				this.setscoreTotal(50);
+				listCoup.remove("grandeSuite");
 
-			this.setScoreSomme(30);
-			this.setscoreTotal(30);
+			} else if (listCoup.contains("petiteSuite") && model.petiteSuite(de.DesGen) == 30) {
 
-		} else if (model.full(de.DesGen) == 25) {
+				this.setScoreSomme(30);
+				this.setscoreTotal(30);
+				listCoup.remove("petiteSuite");
 
-			this.setScoreSomme(25);
-			this.setscoreTotal(25);
+			} else if (listCoup.contains("full") && model.full(de.DesGen) == 25) {
 
-		} else if (model.carre(de.DesGen) != 0) {
+				this.setScoreSomme(25);
+				this.setscoreTotal(25);
+				listCoup.remove("full");
 
-			this.setScoreSomme(model.carre(de.DesGen));
-			this.setscoreTotal(model.carre(de.DesGen));
+			} else if (listCoup.contains("carre") && model.carre(de.DesGen) != 0) {
 
-		} else if (model.brelan(de.DesGen) != 0) {
+				this.setScoreSomme(model.carre(de.DesGen));
+				this.setscoreTotal(model.carre(de.DesGen));
+				listCoup.remove("carre");
 
-			this.setScoreSomme(model.carre(de.DesGen));
-			this.setscoreTotal(model.carre(de.DesGen));
+			} else if (listCoup.contains("brelan") && model.brelan(de.DesGen) != 0) {
 
-		} else {
+				this.setScoreSomme(model.carre(de.DesGen));
+				this.setscoreTotal(model.carre(de.DesGen));
+				listCoup.remove("brelan");
+
+			} else {
+
+			}
 
 		}
-
-
 	}
 
 
